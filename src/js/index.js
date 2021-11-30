9ff633855c23e37004856af13565d37e9315ee55
TheLuisAbarca <arclaav.adm@gmail.com>
2021-11-30 13:16:22 -0500

    Set an array of tasks, containing three keys each task.

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const tasks = [
+    {
+        id: 1,
+        description: 'Go to the store',
+        completed: false
+    },
+    {
+        id: 2,
+        description: 'Finish my screencast',
+        completed: false
+    },
+    {
+        id: 3,
+        description: 'Clean the house',
+        completed: true
+    },
+    {
+        id: 4,
+        description: 'Mow the lawn',
+        completed: false
+    }
+];
