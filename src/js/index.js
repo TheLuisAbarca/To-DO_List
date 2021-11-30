5e5b37373cd3dbaa92868294a2f7212274c55422
TheLuisAbarca <arclaav.adm@gmail.com>
2021-11-30 17:43:11 -0500

    Add style to the corresponding task list.

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,11 @@
+import '@fortawesome/fontawesome-free/js/fontawesome'
+import '@fortawesome/fontawesome-free/js/solid'
+import '@fortawesome/fontawesome-free/js/regular'
+import '@fortawesome/fontawesome-free/js/brands'
+
+import '../css/bootstrap.min.css';
+import '../css/style.css';
+
 const tasks = [
     {
         index: 1,
@@ -24,11 +32,13 @@ const tasks = [
 
  tasks.forEach(task => {
      const taskItem = document.createElement('li');
-     taskItem.className = 'task-item';
+     taskItem.className = 'task-item row p-2 justify-content-center align-items-center';
      taskItem.innerHTML = `
-         <input type="checkbox" class="task-checkbox" ${task.completed ? 'checked' : ''}>
-         <span class="task-description">${task.description}</span>
-         <button class="delete-task">Delete</button>
+         <input class="col-1" type="checkbox" class="task-checkbox" ${task.completed ? 'checked' : ''}>
+         <span class="col-9 task-description">${task.description}</span>
+         <button class="col-2 text-muted option-Task">
+            <i class="fas fa-ellipsis-v"></i>
+         </button>
      `;
      taskList.appendChild(taskItem);
  });
