2b837b696c152d4906bd9114e28747428d764e4a
TheLuisAbarca <arclaav.adm@gmail.com>
2021-12-01 17:12:48 -0500

    Store updated array of items on local storage.

--- src/js/statusModule.js
+++ src/js/statusModule.js
@@ -3,9 +3,16 @@ class StatusCompleted {
     static checked(obj) {
         if (obj.chkbxs.checked) {
             obj.list[obj.id].completed = true;
+            this.LS_update(obj.list);
         } else {
             obj.list[obj.id].completed = false;
+            this.LS_update(obj.list);
         }
+    }
+
+    static LS_update(list) {
+        const ls_data = JSON.stringify(list);
+        localStorage.setItem('To-Do_Tasks', ls_data);
     }
 
     static completedTDList(list) {
