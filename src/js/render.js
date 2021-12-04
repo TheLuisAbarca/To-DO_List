2085e0a8da7b32fedaef8f45324bc7bcfe7199ab
TheLuisAbarca <arclaav.adm@gmail.com>
2021-12-04 17:37:00 -0500

    Add Style to button and first try of draggable items for mobile.

--- src/js/render.js
+++ src/js/render.js
@@ -15,6 +15,28 @@ function ChangeTaskPosition() {
   }
 }
 
+function TouchMove(e,taskItem) {
+  e.preventDefault();
+  /*const touch = event.touches[0];
+  const target = document.elementFromPoint(touch.clientX, touch.clientY);
+  if (target.classList.contains('task-item')) {
+    newIndex = Array.from(taskList.children).indexOf(target);
+    moveIndex = Array.from(taskList.children).indexOf(event.target);
+  }*/
+  const touchLocation = e.targetTouches[0];
+  taskItem.style.left = `${touchLocation.pageX}px`;
+  taskItem.style.top = `${touchLocation.pageY}px`;
+}
+
+function TouchEnd(taskItem) {
+  /*
+  const x = event.changedTouches[0].clientX;
+  const y = event.changedTouches[0].clientY;
+  */
+ const x = parseInt(taskItem.style.left);
+ const y = parseInt(taskItem.style.top);
+}
+
 function renderTask(task, index) {
   task.index = index;
   const taskItem = document.createElement('li');
@@ -25,7 +47,7 @@ function renderTask(task, index) {
   taskItem.innerHTML = `
          <input class="col-1 task-checkbox" type="checkbox" ${task.completed ? 'checked' : ''}>
          <input class="text col-9 task-description text-${task.index}" type="text" value ="${task.description}">
-         <button class="col-1 text-muted delete-Task">
+         <button class="col-1 text-muted delete-Task border border border-1 rounded-pill">
           <i class="fas fa-trash-alt"> </i>
          </button>
          <button class="col-1 text-muted option-Task">
@@ -36,6 +58,9 @@ function renderTask(task, index) {
   taskItem.addEventListener('dragstart', () => { moveIndex = index; });
   taskItem.addEventListener('dragover', () => { newIndex = index; });
   taskItem.addEventListener('dragend', ChangeTaskPosition); // eslint-disable-line no-use-before-define
+  taskItem.addEventListener('touchmove', (e) => {TouchMove(e, taskItem)});
+  //taskItem.addEventListener('touchstart', () => { moveIndex = index; });
+  taskItem.addEventListener('touchend', TouchEnd(taskItem));
   taskList.appendChild(taskItem);
 }
 
