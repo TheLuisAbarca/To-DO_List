2ae3bc5ed0cae030113875277e43f264d9768f4c
TheLuisAbarca <arclaav.adm@gmail.com>
2021-12-09 22:37:03 -0500

    Fix Javascript Linter Errors.

--- src/js/part2.js
+++ src/js/part2.js
@@ -1,12 +1,13 @@
-import { SetOnLocalStorage,GetFromLocalStorage } from './LocalStorage.js';
-let tasks; 
+import { GetFromLocalStorage } from './LocalStorage.js';
+
+let tasks;
 function mockchecked(obj) {
   const list = GetFromLocalStorage();
   if (obj.chkbxs.checked) {
     list[obj.id].completed = true;
   } else {
     list[obj.id].completed = false;
-  } 
+  }
   return list;
 }
 
--- src/js/part2.test.js
+++ src/js/part2.test.js
@@ -1,17 +1,19 @@
-import { SetOnLocalStorage,GetFromLocalStorage } from './LocalStorage.js';
+import { SetOnLocalStorage } from './LocalStorage.js';
 import { renderTask } from './render.js';
 import { htmlTemplate, LocalStorageMock } from './toolsTest.js';
-import { mockchecked, cleanObjects, editObjects, clearAllCompleted } from './part2.js';
+import {
+  mockchecked, cleanObjects, editObjects,
+} from './part2.js';
 
 describe('Testing Part2 Task', () => {
-  const mockedlocalStorage = new LocalStorageMock();  
+  const mockedlocalStorage = new LocalStorageMock();
   describe('Testing Status "Completed"', () => {
     test('should return true if the task is checked', () => {
       const tasks = JSON.parse(mockedlocalStorage.getItem('tasks')) || [];
       const basicBody = document.createElement('div');
       basicBody.innerHTML = htmlTemplate;
       document.body.appendChild(basicBody);
-      const taskElement = { index: tasks.length, description: 'Task 1 description', completed: false};
+      const taskElement = { index: tasks.length, description: 'Task 1 description', completed: false };
       tasks.push(taskElement);
       mockedlocalStorage.setItem('tasks', JSON.stringify(tasks));
       SetOnLocalStorage(tasks);
@@ -23,36 +25,34 @@ describe('Testing Part2 Task', () => {
       mockedlocalStorage.setItem('tasks', JSON.stringify(mockchecked(baseObj)));
       const result = JSON.parse(mockedlocalStorage.getItem('tasks'));
       expect(result[0].completed).toBe(true);
-    });    
-  });  
-        
+    });
+  });
+
   describe('Testing "Clear Completed"', () => {
     test('should clean all the tasks that are market', () => {
-      const objectsToClean = [ { index: 0, description: 'Task 1 description', completed: true }, 
-      { index: 1, description: 'Task 1 description', completed: false },
-      { index: 2, description: 'Task 1 description', completed: true },
+      const objectsToClean = [{ index: 0, description: 'Task 1 description', completed: true },
+        { index: 1, description: 'Task 1 description', completed: false },
+        { index: 2, description: 'Task 1 description', completed: true },
       ];
       mockedlocalStorage.setItem('tasks', JSON.stringify(objectsToClean));
       SetOnLocalStorage(objectsToClean);
-      const tasks = JSON.parse(mockedlocalStorage.getItem('tasks')) || [];
       const basicBody = document.createElement('div');
       basicBody.innerHTML = htmlTemplate;
       document.body.appendChild(basicBody);
       const clean = cleanObjects();
       expect(clean.length).toBe(1);
     });
-  });  
-  describe('Testing Editing tasks', () =>{
-    test('Should test the editing task Object', () =>{
-      const objectsToEdit = [ { index: 0, description: 'Task 1 description', completed: true } ];
+  });
+  describe('Testing Editing tasks', () => {
+    test('Should test the editing task Object', () => {
+      const objectsToEdit = [{ index: 0, description: 'Task 1 description', completed: true }];
       mockedlocalStorage.setItem('tasks', JSON.stringify(objectsToEdit));
       SetOnLocalStorage(objectsToEdit);
       renderTask(objectsToEdit[0], 0);
-      const inputs = document.getElementById(`text-${index}`);
+      const inputs = document.getElementById('text-0');
       inputs.value = 'Edited Task';
       const editing = editObjects(0);
       expect(editing[0].description).toBe('Edited Task');
     });
   });
-
 });
